9e95d449f4aeac7f0642018f79be5642a86e5ea7
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-04-23 19:25:36 +0200

    Don't use localtime , use localtime_s instead

--- libs/include/ThreadedLoggerForCPP/LoggerThread.hpp
+++ libs/include/ThreadedLoggerForCPP/LoggerThread.hpp
@@ -197,9 +197,13 @@ private:
     auto now = std::chrono::system_clock::now();
     auto in_time_t = std::chrono::system_clock::to_time_t(now);
 
-    std::stringstream ss;
-    ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d-%H-%M-%S");
-    return ss.str();
+    struct tm timeinfo;
+    localtime_s(&timeinfo, &in_time_t);
+
+    char buffer[80];
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d-%H-%M-%S", &timeinfo);
+
+    return std::string(buffer);
   }
 };
 
