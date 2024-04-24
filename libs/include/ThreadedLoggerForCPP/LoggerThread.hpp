fec424a1c50666756e6b605446b798e2eaf03b90
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-04-24 16:09:41 +0200

    Avoid concatanation errors while using const
    Avoid concatanation errors while using const by adding a new synthax to logMessageAsync and making the old synthax deperecated

--- libs/include/ThreadedLoggerForCPP/LoggerThread.hpp
+++ libs/include/ThreadedLoggerForCPP/LoggerThread.hpp
@@ -22,7 +22,7 @@
 enum class LogLevel { INFO, WARNING, ERRORING, LOGICERROR };
 
 class LoggerThread {
-public:
+ public:
   LoggerThread() : Done_Logger_Thread(false), AppClosing(false) {
     std::thread workerThread(&LoggerThread::logWorker, this);
     workerThread.detach();
@@ -32,15 +32,33 @@ public:
     {
       std::unique_lock<std::mutex> lock(mtx);
       Done_Logger_Thread = true;
-      Unlock_Logger_Thread.notify_one(); // Notify worker thread to stop
+      Unlock_Logger_Thread.notify_one();  // Notify worker thread to stop
     }
     if (logFile.is_open()) {
       logFile.close();
     }
   }
 
-  void logMessageAsync(LogLevel level, const std::string &sourceFile, int line,
-                       const std::string &message) {
+  [[deprecated(
+      "Please use the new syntax: YourloggerInstance.logMessageAsync(level, __FILE__, __LINE__, "
+      "{message}); instead of YourloggerInstance.logMessageAsync(level, __FILE__, __LINE__, "
+      "message);")]] void
+  logMessageAsync(
+      LogLevel level, const std::string &sourceFile, int line, const std::string &message) {
+    logMessageAsync(level, sourceFile, line, {message});
+  }
+
+  void logMessageAsync(
+      LogLevel level,
+      const std::string &sourceFile,
+      int line,
+      const std::initializer_list<std::string> &messageParts) {
+    std::ostringstream messageStream;
+    for (const auto &part : messageParts) {
+      messageStream << part;
+    }
+    std::string message = messageStream.str();
+
     std::unique_lock<std::mutex> lock(mtx);
     tasks.emplace([=] { logMessage(level, sourceFile, line, message); });
     Unlock_Logger_Thread.notify_one();
@@ -56,9 +74,9 @@ public:
         tasks.pop();
         task();
       }
-      Unlock_Logger_Thread.notify_one(); // Notify worker thread to stop
+      Unlock_Logger_Thread.notify_one();  // Notify worker thread to stop
     }
-    logFile.close(); // Close the log file
+    logFile.close();  // Close the log file
 
     // After the thread has finished, perform cleanup tasks
     TimeStamp = getTimestamp();
@@ -67,10 +85,11 @@ public:
     this->copyFile(src, dst);
     AppClosing = true;
   }
-  void StartLoggerThread(const std::string &LogFolderPath,
-                         const std::string &LogFilePath,
-                         const std::string &LogFolderBackupPath,
-                         const std::string &LogFileBackupPath) {
+  void StartLoggerThread(
+      const std::string &LogFolderPath,
+      const std::string &LogFilePath,
+      const std::string &LogFolderBackupPath,
+      const std::string &LogFileBackupPath) {
     this->LogFolderPathForTheThread = LogFolderPath;
     this->LogFilePathForTheThread = LogFilePath;
     this->logFilePath_ = LogFilePath;
@@ -86,7 +105,7 @@ public:
   std::thread &getGlobalLogThread() { return LogThread; }
   std::thread LogThread;
 
-private:
+ private:
   std::queue<std::function<void()>> tasks;
   std::mutex mtx;
   std::condition_variable Unlock_Logger_Thread;
@@ -125,37 +144,34 @@ private:
   }
 
   template <typename... Args>
-  void logMessage(LogLevel level, const std::string &sourceFile, int line,
-                  const Args &...args) {
+  void logMessage(LogLevel level, const std::string &sourceFile, int line, const Args &...args) {
     std::ostringstream oss;
     switch (level) {
-    case LogLevel::INFO:
-      oss << "[INFO] ";
-      break;
-    case LogLevel::WARNING:
-      oss << "[WARNING] ";
-      break;
-    case LogLevel::ERRORING:
-      oss << "[ERROR] ";
-      break;
-    case LogLevel::LOGICERROR:
-      oss << "[LOGIC ERROR] ";
-      break;
+      case LogLevel::INFO:
+        oss << "[INFO] ";
+        break;
+      case LogLevel::WARNING:
+        oss << "[WARNING] ";
+        break;
+      case LogLevel::ERRORING:
+        oss << "[ERROR] ";
+        break;
+      case LogLevel::LOGICERROR:
+        oss << "[LOGIC ERROR] ";
+        break;
     }
-    oss << getTimestamp() << " [" << extractRelativePath(sourceFile) << ":"
-        << line << "] ";
+    oss << getTimestamp() << " [" << extractRelativePath(sourceFile) << ":" << line << "] ";
     append(oss, args...);
     std::string message = oss.str();
     std::cout << message << std::endl;
     std::ofstream logFile(logFilePath_, std::ios::app);
-    logFile << message << std::endl; // Write to file
+    logFile << message << std::endl;  // Write to file
   }
 
   std::string extractRelativePath(const std::string &filePath) {
     size_t found = filePath.find_last_of("/\\");
     if (found != std::string::npos) {
-      size_t srcIndex =
-          filePath.rfind(LoggerGlobals::SrcProjectDirectory, found);
+      size_t srcIndex = filePath.rfind(LoggerGlobals::SrcProjectDirectory, found);
       if (srcIndex != std::string::npos) {
         return filePath.substr(srcIndex);
       }
@@ -164,7 +180,8 @@ private:
     return filePath;
   }
 
-  template <typename T> void append(std::ostringstream &oss, const T &arg) {
+  template <typename T>
+  void append(std::ostringstream &oss, const T &arg) {
     oss << arg;
   }
 
@@ -183,12 +200,11 @@ private:
 
     std::ofstream dst(dest, std::ios::binary);
     if (!dst.is_open()) {
-      std::cerr << "Error: Unable to create or open destination file " << dest
-                << ".\n";
+      std::cerr << "Error: Unable to create or open destination file " << dest << ".\n";
       src.close();
       return;
     };
-    dst << src.rdbuf(); // Efficiently copy file
+    dst << src.rdbuf();  // Efficiently copy file
     src.close();
     dst.close();
   }
@@ -207,4 +223,4 @@ private:
   }
 };
 
-#endif // LOGGER_THREAD_HPP
+#endif  // LOGGER_THREAD_HPP
